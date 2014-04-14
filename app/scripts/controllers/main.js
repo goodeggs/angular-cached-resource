f81e0e78814c3e74e475d67c60463573462faafb
Max Edmands <me@maxedmands.com>
2014-04-14 09:08:23 -0700

    Some further work

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
 angular.module('angularCachedResourceApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $state) {
+    $scope.$state = $state;
   });
